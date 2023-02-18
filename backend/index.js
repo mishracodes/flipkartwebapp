3cdffe004a0730035554c9e14790c9b157165106
Amit Kumar Mishra <31859346+mishracodes@users.noreply.github.com>
2023-02-19 01:46:28 +0530

    added healthcheck api endpoint

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors())
 app.use(express.json())
 
+app.get('/api/healthcheck',(req,res)=>{
+  console.log("Health Check Success 200 OK")
+  res.send({  healthCheck: "success 200 OK"  })
+})
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/categories', require('./routes/categories'))
 app.use('/api/products', require('./routes/products'))
